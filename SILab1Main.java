49bc88a06e691bed42044ac1a5ced8eeb9049570
stefanovak51 <109036969+stefanovak51@users.noreply.github.com>
2024-04-13 15:22:31 +0200

    Resloved merge conflict in SILab1Main.java

--- SILab1Main.java
+++ SILab1Main.java
@@ -13,6 +13,9 @@ public class SILab1Main {
         for (int i = 0; i < n; i++) {
             list.add(sc.next());
         }
+<<<<<<< HEAD
       System.out.println(SILab1_1.filterOddLengthStrings(list));
+=======
+>>>>>>> 45b56d2 (Call method makeBigStringFrom in main class)
     }
 }
